ac3dc2fbdb468826f5744447d1c0797f0bfc06c3
megansmithcreative <megansmith.creative@gmail.com>
2026-03-31 18:19:29 +0500

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
+let cart = [];
 
+function addToCart(name, price) {
+  cart.push({name, price});
+  updateCart();
+}
+
+function updateCart() {
+  let list = document.getElementById("cartItems");
+  let count = document.getElementById("count");
+  let total = 0;
+
+  list.innerHTML = "";
+
+  cart.forEach(item => {
+    let li = document.createElement("li");
+    li.innerText = item.name + " - Rs " + item.price;
+    list.appendChild(li);
+    total += item.price;
+  });
+
+  count.innerText = cart.length;
+  document.getElementById("total").innerText = "Total: Rs " + total;
+}
+
+function toggleCart() {
+  let cartBox = document.getElementById("cartBox");
+  cartBox.style.display = cartBox.style.display === "block" ? "none" : "block";
+}
+
+function checkout() {
+  let message = "Order:\n";
+
+  cart.forEach(item => {
+    message += item.name + " - Rs " + item.price + "\n";
+  });
+
+  window.open("https://wa.me/92XXXXXXXXXX?text=" + encodeURIComponent(message));
+}
